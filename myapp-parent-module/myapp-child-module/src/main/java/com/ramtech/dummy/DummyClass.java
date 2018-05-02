61423bed8067b6e9fd4ff3e967473beba6618e94
ramtech123 <mogasale.tech@gmail.com>
2018-05-02 16:28:06 +0530

    A parent and child maven module with pmd checkstyle configuration

---
+++ myapp-parent-module/myapp-child-module/src/main/java/com/ramtech/dummy/DummyClass.java
@@ -0,0 +1,14 @@
+package com.ramtech.dummy;
+
+/**
+ * This is a dummy class.
+ */
+public class DummyClass {
+
+    /**
+     * This is a dummy method.
+     */
+    public void dummyMethod() {
+        System.out.println("This is dummmy method in a dummy class!");
+    }
+}
